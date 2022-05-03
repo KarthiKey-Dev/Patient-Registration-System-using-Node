f405464164930de09df54c1a95116c20034d707e
karthikeyan <58414583+karthiktrp@users.noreply.github.com>
2022-05-03 22:50:41 +0530

    Update loginRoutes.js

--- routes/loginRoutes.js
+++ routes/loginRoutes.js
@@ -1,7 +1,7 @@
 /** @format */
 const express = require("express");
 const Model = require("../model/registrationModel");
-bcrypt = require("bcrypt");
+// bcrypt = require("bcrypt");
 const router = express.Router();
 
 //Post Method
@@ -9,20 +9,21 @@ router.post("/auth", async (req, res) => {
   const data = await Model.find();
 
   try {
-    const user = await data.findOne({ user_name: req.body.user_name });
+    const user = await Model.findOne({ user_name: req.body.user_name });
     if (user) {
-      const cmp = await bcrypt.compare(
-        req.body.user_password,
-        user.user_password
-      );
+      // const cmp = await bcrypt.compare(
+      //   req.body.user_password,
+      //   user.user_password
+      // );
       // const isMatch = user.user_password === req.body.user_password;
+      const cmp = req.body.user_password === user.user_password;
       if (cmp) {
         res.send("Auth Successful");
       } else {
-        res.send("Wrong password.");
+        res.send("invalid password.");
       }
     } else {
-      res.send("Wrong username ");
+      res.send("invalid username ");
     }
   } catch (error) {
     res.status(500).json({ message: "something went wrong" });
