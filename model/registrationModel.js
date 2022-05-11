4230931ca44a34e30f778e8cbd367eafb648ff99
karthikeyan <58414583+karthiktrp@users.noreply.github.com>
2022-05-11 19:22:47 +0530

    Update registrationModel.js

--- model/registrationModel.js
+++ model/registrationModel.js
@@ -2,30 +2,33 @@
 
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  user_name: {
-    required: true,
-    type: String,
-    unique: true 
+const userSchema = new mongoose.Schema(
+  {
+    user_name: {
+      required: true,
+      type: String,
+      unique: true,
+    },
+    user_password: {
+      required: true,
+      type: String,
+    },
+    user_org: {
+      // required: true,
+      type: String,
+    },
+    user_position: {
+      // required: true,
+      type: String,
+    },
+    user_type: {
+      required: true,
+      type: String,
+    },
   },
-  user_password: {
-    required: true,
-    type: String,
-  },
-  user_org: {
-    // required: true,
-    type: String,
-  },
-  user_position: {
-    // required: true,
-    type: String,
-  },
-  user_type: {
-    // required: true,
-    type: String,
-  },
-},{
-  timestamps: true
-});
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = mongoose.model("user", userSchema);
