582be8c4a0bac79594cf659af29002dfc89a6603
karthikeyan <58414583+karthiktrp@users.noreply.github.com>
2022-05-21 14:35:27 +0530

    Update registrationModel.js

--- model/registrationModel.js
+++ model/registrationModel.js
@@ -43,7 +43,7 @@ const userSchema = new mongoose.Schema(
     user_password: {
       type: String,
       // default: randomstring.generate(10),
-      default: "1234",
+      default: "123456",
     },
   },
   {
