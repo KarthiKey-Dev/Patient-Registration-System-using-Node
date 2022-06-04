ef947039a543b5a884a708218c7937c5c95766bb
karthikeyan <58414583+karthiktrp@users.noreply.github.com>
2022-06-04 22:29:13 +0530

    Update loginRoutes.js

--- routes/loginRoutes.js
+++ routes/loginRoutes.js
@@ -1,6 +1,7 @@
 /** @format */
 const express = require("express");
 const Model = require("../model/registrationModel");
+const Model2 = require("../model/formTemplateModel");
 // bcrypt = require("bcrypt");
 const router = express.Router();
 
@@ -22,15 +23,15 @@ router.post("/auth", async (req, res) => {
       //   user.user_password
       // );
       // const isMatch = user.user_password === req.body.user_password;
+      const FormTemplate = await Model2.find();
       const cmp = req.body.user_password === user.user_password;
       if (cmp) {
-        res
-          .status(200)
-          .json({
-            message: "Logged In Successfully.",
-            User: user,
-            AccessToken: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9",
-          });
+        res.status(200).json({
+          message: "Logged In Successfully.",
+          User: user,
+          AccessToken: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9",
+          form_Templates: FormTemplate,
+        });
       } else {
         res.status(400).json({ message: "invalid password." });
       }
